4735d51f5f7ad0c37023c809fc6f5d73742d620f
vb15008 <isidroalverto1995@hotmail.com>
2018-04-03 23:01:11 -0600

    Prueba

---
+++ movimiento/build/CMakeFiles/movi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/movi.dir/movimiento.cpp.o"
+  "movi.pdb"
+  "movi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/movi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
